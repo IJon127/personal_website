774e37b34789786d7d3680e976fcfef1ded5ddd1
Ijon Hsieh <ih2168@nyu.edu>
2022-02-10 00:23:53 -0500

    write sth. in js

---
+++ script.js
@@ -0,0 +1,10 @@
+// let navBurgerBtn = document.querySelector(#nav_burger);
+// let navBurgerBtnActive = false;
+// navBurgerBtn.addEventListener('click', openNav);
+
+// function openNav(){
+
+//     if (navBurgerBtnActive === false) {
+
+//     }
+// }
